34dc3e5a954f33da286bc05af5e9f8a078eed1be
SFerFer1 <10048@holbertonstudents.com>
2024-12-19 15:01:10 +0000

    save

--- arguments_0.c
+++ arguments_0.c
@@ -3,12 +3,12 @@
 
 int main(int ac, char **av)
 {
-int i = 0;
+	int i = 0;
 
-while (av[i] != NULL)
-{
-printf("%s\n", av[i]);
-i++;
-}
-return(0);
+	while (av[i] != NULL)
+	{
+		printf("%s\n", av[i]);
+		i++;
+	}
+	return(0);
 }
